6f74ecbffc2c4cdc3838ac3dfeaa7a01c2e37fff
NotFully <127725127+NotFully@users.noreply.github.com>
2023-05-09 22:53:05 +0500

    Update config.py

--- notification_test_task/mailings/config.py
+++ notification_test_task/mailings/config.py
@@ -1,2 +1,2 @@
-TKN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTQ4MjkwOTMsImlzcyI6ImZhYnJpcXVlIiwibmFtZSI6Imh0dHBzOi8vdC5tZS9uaWxsb3JfciJ9.IUCuehOdksul7FtaN9ETR8NZ-PwtEJ09d8rUqlDX19A"
+TKN = token
 URL = "https://probe.fbrq.cloud/v1/send/"
